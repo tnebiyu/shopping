9e427ea9c838afcbaa015aeeb1ca096ac0b52fbf
Nebiyu <nebiyu28@gmail.com>
2023-02-09 20:26:20 +0300

    data model file modified

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -10,22 +10,22 @@ class Product{
   var color;
 
   Product({required this.id,required this.name,required this.color, required this.price} );
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+        id: json['id'] as int,
+        name: json['name'] as String,
+        price: json['price'] as double,
+         color: json['color'] as String);
+  }
 }
-Product product = new Product(id: 4, name: 'orange4',color: Colors.blueGrey, price: 3.5);
-Product product1 = new Product(id: 2, name: 'Orange2',color: Colors.blueGrey, price: 4);
-Product product2 = new Product(id: 3, name: 'Orange3',color: Colors.blueGrey, price: 4.5);
-Product produc3 = new Product(id: 1, name: "organge",color: Colors.blueGrey, price: 3);
 
-class Category{
+class Catagory{
   String name;
   List<Product> products;
 
-  Category({required this.name,required this.products});
+  Catagory({required this.name,required this.products});
+
 }
-Category category = new Category(name: 'Menu1', products: [product,product1,produc3, product2]);
-Category category2 = new Category(name: 'Menu2', products: [product,product1,produc3, product2]);
-Category categor3 = new Category(name: 'Menu3', products: [product,product1,produc3, product2]);
-Category categor4 = new Category(name: 'Menu4', products: [product,product1,produc3, product2]);
 
 class ItemCart{
   Product product;
