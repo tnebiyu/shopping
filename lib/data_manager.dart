05c41dd2993231d15ab47af97736d75dd82f5d0c
Nebiyu <nebiyu28@gmail.com>
2023-02-09 20:24:23 +0300

    modifiy data manager
    the name category was the mistake i solved it

--- lib/data_manager.dart
+++ lib/data_manager.dart
@@ -1,5 +1,31 @@
 import 'package:shopping/data_model.dart';
 class DataManager{
-  List<Category>? _menu;
+  List<Catagory>? _menu;
   List<ItemCart>cart = [];
+
+  getMenu(){
+    return _menu;
+  }
+  cartAdd(Product p){
+    bool found = false;
+    for(var item in cart){
+      if(item.product.id == p.id){
+        item.quanitity++;
+        found =true;
+      }
+    }
+  }
+  cartRemove(Product p){
+    cart.removeWhere((element) => element.product.id == p.id);
+  }
+  cartClear(){
+    cart.clear();
+  }
+  double cartTotal(){
+    double total =0;
+    for(var item in cart){
+       total += item.quanitity * item.product.price;
+    }
+return total;
+  }
 }
