52d8bd9d5b91145bdcf9eddbab815aa51c113d4d
Nebiyu <nebiyu28@gmail.com>
2023-02-09 20:25:33 +0300

    screen directory
    screen home and menu_page added

---
+++ lib/Screen/home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/Screen/menu_page.dart';
+import 'package:shopping/data_manager.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ // const Home({Key? key}) : super(key: key);
+  int _currentIndex =0;
+
+  @override
+  Widget build(BuildContext context) {
+    var dataManager = DataManager();
+    late Widget currentPage;
+    switch (_currentIndex){
+      case 0:
+        currentPage = Menu(dataManager: dataManager);
+        break;
+      case 1:
+        currentPage = Menu(dataManager: dataManager);
+        break;
+      case 2:
+        currentPage = Menu(dataManager: dataManager);
+    }
+    return Scaffold(
+      appBar: AppBar(title:Text( 'Ariob Shopping App'),),
+      body: currentPage,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: Colors.red,
+        onTap: (index){
+          setState(() {
+            _currentIndex =index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_travel), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.flag), label: 'Flag'),
+
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screen/menu_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:shopping/data_manager.dart';
+
+import '../data_model.dart';
+class Menu extends StatelessWidget {
+  DataManager? dataManager;
+
+
+  Menu({required this.dataManager});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder<List<Catagory>>(
+          future: dataManager!.getMenu(),
+          builder:(context, snapshot){
+            if(snapshot.hasData){
+              var categories = snapshot.data! as List<Catagory>;
+return ListView.builder(
+    itemCount: categories.length,
+    itemBuilder: (context, index){
+      return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+    Padding(
+    padding: const EdgeInsets.only(
+    top: 32.0, bottom: 8.0, left: 8.0),
+    child: Text(
+    categories[index].name,
+    style: TextStyle(color: Colors.brown.shade400),
+    ),
+    ),
+    ListView.builder(
+    shrinkWrap: true,
+    physics: const ClampingScrollPhysics(),
+    itemCount: categories[index].products.length!,
+    itemBuilder: (context, proIndex) {
+    var product =categories[index].products[proIndex];
+    return MenuItem(
+    product: product,
+    onAdd: (addedProduct) => dataManager!.cartAdd(addedProduct),
+    );
+    },
+    )
+
+    ],
+    );
+    });
+
+            }
+            else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            return CircularProgressIndicator();
+          }),
+    );
+  }
+}
+class MenuItem extends StatelessWidget {
+  final Product product;
+  final Function onAdd;
+  const MenuItem({Key? key, required this.product, required this.onAdd})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(4.0),
+      child: Card(
+        elevation: 4,
+        child: Column(
+          children: [
+            Card(
+              color: product.color,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SelectableText(
+                        product.name,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text("\$${product.price.toStringAsFixed(2)} ea"),
+                    ],
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        onAdd(product);
+                      },
+                      child: const Text("Add"))
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
